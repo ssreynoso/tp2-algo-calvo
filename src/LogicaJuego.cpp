f8c6bf3064944e15d789dc7bc7138f9442fc6401
mlalia <149947765+mlalia@users.noreply.github.com>
2023-11-10 11:33:02 -0300

    Update LogicaJuego.cpp

--- src/LogicaJuego.cpp
+++ src/LogicaJuego.cpp
@@ -1,8 +1,9 @@
 #include "TesoroBinario.h"
 
 void TesoroBinario::juego() {
-    this->jugadores->iniciarCursor();
+    this->jugadores->iniciarCursor();    //El cursor se inicia en NULL
     this->jugadores->ligarUltimoConPrimero();
+    this->jugadores->avanzarCursor();    //Se avanza el cursor al primero
     while (!finDeJuego()) {
         Jugador* jugadorActual = this->jugadores->obtenerCursor();
         if (sigueJugando(jugadorActual)) {
@@ -22,8 +23,8 @@ bool TesoroBinario::finDeJuego() {
     // El juego termina cuando todos los jugadores se quedan sin tesoros salvo uno, ganando este jugador
     int jugadoresConTesoro = 0;
 
-    // Recorro todos los jugadores.
-    for (int i = 0; i < this->cantidadJugadores; i++) {
+    // Recorro todos los jugadores. El indice de la lista va de 1 a cantidadJugadores
+    for (int i = 1; i <= this->cantidadJugadores; i++) {
         int numeroTesoros = this->jugadores->obtener(i)->getTesoros();
         // Tiene tesoros?
         if (numeroTesoros > 0) {
